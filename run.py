0d2568a15536a64b8c4249313a58769c6b53ddd1
Nur Imam Masri <nurimammasri.01@gmail.com>
2021-03-16 09:30:50 +0800

    add code to upload desc fruits catalog run.py

---
+++ run.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import os
+
+import requests
+
+# external ip address for qwiklabs
+external_ip = "34.70.247.156"
+url = "http://{}/fruits/".format(external_ip)
+
+# depending on windows use below
+images_dir = os.path.join("supplier-data", "images", "")
+desc_dir = os.path.join("supplier-data", "descriptions", "")
+
+# depending on linux use below
+# images_dir = os.path.join(os.sep, "supplier-data", "images", "")
+# desc_dir = os.path.join(os.sep, "supplier-data", "images", "")
+
+# list name file in /data/feedback
+images_list = image_list = [item for item in os.listdir(images_dir) if item.endswith(".jpeg")]
+desc_list = os.listdir(desc_dir)
+
+
+def prosess_text():
+    """
+    Used to perform the process of changing from file.txt to a list dictionary
+    :rtype: list
+    :return: list of dict text
+    """
+    list_dict = []
+    key = ['name', 'weight', 'description', 'image_name']
+    for file_index, name_file in enumerate(desc_list):
+        file_dir = os.path.join(desc_dir, name_file)
+        with open(file_dir) as file:
+            text_list = [n.strip() for n in file]
+            text_dict = {}
+            for index, value in enumerate(text_list):
+                if key[index] == 'image_name':
+                    text_dict[key[index]] = images_list[file_index]
+                elif key[index] == 'weight':
+                    value = value.replace(" lbs", "")
+                    text_dict[key[index]] = int(value)
+                else:
+                    text_dict[key[index]] = value
+            list_dict.append(text_dict)
+    return list_dict
+
+
+def post_request():
+    """
+    Performs HTTP protocol to post dict data requests to the url from the extracted file.txt
+    """
+    p = prosess_text()
+    for data in p:
+        response = requests.post(url, json=data)
+        if response.ok and response.status_code == 201:
+            print("Success")
+        else:
+            response.raise_for_status()
+            raise Exception("GET failed with status code {}".format(response.status_code))
+
+
+if __name__ == '__main__':
+    post_request()
